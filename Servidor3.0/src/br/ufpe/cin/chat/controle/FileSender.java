a854c38ce9260dea496376314cbe05244da2e077
Adilson Angelo da Silva Junior <aasj2@WINDOWS.CIN.UFPE.BR>
2016-12-14 11:00:39 -0300

    modificação no FileSender do servidor

--- Servidor3.0/src/br/ufpe/cin/chat/controle/FileSender.java
+++ Servidor3.0/src/br/ufpe/cin/chat/controle/FileSender.java
@@ -38,7 +38,7 @@ public class FileSender implements Runnable {
 			int contador = 0;
 			progressBar.setValue(0);
 			progressBar.setMaximum((int)tamanho);
-			while((counter = buffIN.read(bytes)) > 0){
+			while((counter = buffIN.read(bytes)) >= 0){
 				saida.write(bytes, 0, counter);
 				contador += counter;
 				progressBar.setValue(contador);
